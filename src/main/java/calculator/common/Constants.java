a223ca9ae36521fa8ceae8ea6afb5521e1526ce5
siwanmoon <simon___@naver.com>
2025-10-15 19:31:21 +0900

    refactor: 디폴트 구분자를 상수 정의 클래스에 추가

---
+++ src/main/java/calculator/common/Constants.java
@@ -0,0 +1,7 @@
+package calculator.common;
+
+import java.util.List;
+
+public class Constants { // 공용 상수, 문자열 선언
+    public static final List<String> DEFAULT_SEPARATOR = List.of(",", ":"); // 디폴트 구분자
+}
--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,18 +1,16 @@
 package calculator.controller;
 
 import java.util.ArrayList;
-
 import calculator.model.CheckCustomSeparator;
+import calculator.common.Constants;
 
 public class CalculatorController {
     public static void cpu(String calculatorInput) { //입력받은 문자열을 model과 view로 전송
-        ArrayList<String> Separator = new ArrayList<>(); // 구분자를 ArrayList로 저장
-        Separator.add(",");
-        Separator.add(":");
-
-        String customSeparator = CheckCustomSeparator.getSeparator(calculatorInput);
+        ArrayList<String> Separator = new ArrayList<>(Constants.DEFAULT_SEPARATOR); // 구분자를 ArrayList로 저장
 
         // 새로운 커스텀 구분자가 존재할시 구분자 목록에 추가
+        String customSeparator = CheckCustomSeparator.getSeparator(calculatorInput);
+
         if(!customSeparator.isEmpty()) {
             Separator.add(customSeparator);
         }
