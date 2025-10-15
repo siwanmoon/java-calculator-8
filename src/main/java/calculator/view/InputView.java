e38ce05ab3c07befcaea84938f5e4c6743bc0b87
siwanmoon <simon___@naver.com>
2025-10-15 14:52:21 +0900

    chore: InputView.getInput 주석 추가

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -3,7 +3,7 @@ package calculator.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    public static String getInput() {
+    public static String getInput() { //계산기에서 문자열을 입력받음
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         return Console.readLine();
     }
