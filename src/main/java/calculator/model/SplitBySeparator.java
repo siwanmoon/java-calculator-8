8022173875607507cbdf5c175fa271b82c85d057
siwanmoon <simon___@naver.com>
2025-10-17 23:04:19 +0900

    fix: 커스텀 구분자 선언만 하고 내용이 입력되지 않았을 경우 나타나는 오류 해결

--- src/main/java/calculator/model/SplitBySeparator.java
+++ src/main/java/calculator/model/SplitBySeparator.java
@@ -9,12 +9,10 @@ public class SplitBySeparator {
         // 디폴트 구분자로 문자열을 나누기 위해 전처리 해줌
         String regex = String.join("|", Constants.DEFAULT_SEPARATOR);
 
-        if (separator.size() > Constants.DEFAULT_SEPARATOR.size()) {
-            // 커스텀 구분자가 존재할 시 //(커스텀 구분자)\n 부분 문자열에서 제거
-            int customSeparatorIndex = inputString.lastIndexOf(Constants.CUSTOM_SEPARATOR_SUFFIX);
-            inputString = inputString.substring(customSeparatorIndex +
-                    Constants.CUSTOM_SEPARATOR_SUFFIX.length());
+        // 커스텀 구분자 부분을 지우고 더할 문자열 부분만 남김
+        inputString = deleteCustomSeparator(inputString);
 
+        if (separator.size() > Constants.DEFAULT_SEPARATOR.size()) {
             // 커스텀 구분자가 존재할 시 디폴트 구분자 전처리에 추가해줌
             String safeCustomSeparator = Pattern.quote(separator.getLast());
             regex = regex + "|" + safeCustomSeparator;
@@ -22,5 +20,19 @@ public class SplitBySeparator {
 
         // 구분자로 문자열을 나눠줌
         return inputString.split(regex);
+    }
+
+    // 커스텀 구분자가 존재할 시 //(커스텀 구분자)\n 부분 문자열에서 제거
+    public static String deleteCustomSeparator (String inputString) {
+        int customSeparatorPrefixIndex = inputString.indexOf(Constants.CUSTOM_SEPARATOR_PREFIX);
+        int customSeparatorSuffixIndex = inputString.lastIndexOf(Constants.CUSTOM_SEPARATOR_SUFFIX);
+
+        if(customSeparatorPrefixIndex != -1 && customSeparatorSuffixIndex != -1) {
+            // 커스텀 구분자의 종료 선언이 시작되는 지점에 종료 선언 문자열의 길이를 더해 입력 문자열을 잘라줌
+            inputString = inputString.substring(customSeparatorSuffixIndex +
+                    Constants.CUSTOM_SEPARATOR_SUFFIX.length());
+        }
+
+        return inputString;
     }
 }
